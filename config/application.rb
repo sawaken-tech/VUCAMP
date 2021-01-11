4e8f7545a8c9729ecb2e04b65c9e60951e67c0b0
sawaken-tech <sawapaca@gmail.com>
2021-01-11 14:52:50 +0900

    devise_ja

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module VUCAMP
       g.test_framework false
     end
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
