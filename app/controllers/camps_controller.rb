7cb3f5f955e2f39d30a16becfb0d20f6bf47692f
sawaken-tech <sawapaca@gmail.com>
2021-01-21 07:29:55 +0900

    camp_create

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -2,4 +2,18 @@ class CampsController < ApplicationController
   def index
     @camps = Camp.all
   end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def create
+    @camp = Camp.new(camp_params)
+  end
+
+  def camp_params
+    params.require(:camp).permit(
+    )
+  end
+
 end
