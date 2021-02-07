d7af063a779bf9a1cff8ef48d1314c69a415898d
sawaken-tech <sawapaca@gmail.com>
2021-02-08 06:59:33 +0900

    show

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,4 +1,7 @@
 class CampsController < ApplicationController
+
+  before_action :set_camp, only: [:show]
+
   def index
     @camps = Camp.all
   end
@@ -10,12 +13,14 @@ class CampsController < ApplicationController
   end
 
   def create
-    binding.pry
     @camp = Camp.create(camp_params)
     unless @camp.valid?
       flash.now[:alert] = @camp.errors.messages
       render :new and return
     end
+  end
+
+  def show
   end
 
   def camp_params
@@ -31,4 +36,7 @@ class CampsController < ApplicationController
     )
   end
 
+  def set_camp
+    @camp = Camp.find(params[:id])
+  end
 end
