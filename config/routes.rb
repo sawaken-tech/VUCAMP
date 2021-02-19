a8d4093637cbe81cbf8728cb878be0a7c74dddd8
sawaken-tech <sawapaca@gmail.com>
2021-02-20 08:31:22 +0900

    topPage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root "camps#index"
+  root "top#index"
+
+  resources :top, only: [:index]
 
   resources :camps do
     collection do
