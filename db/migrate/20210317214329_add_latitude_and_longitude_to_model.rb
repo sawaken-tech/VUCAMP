e43c2e2d42cdc2165888f3917c1af16d97d8e37a
sawaken-tech <sawapaca@gmail.com>
2021-03-21 07:51:43 +0900

    fix re

--- db/migrate/20210317214329_add_latitude_and_longitude_to_model.rb
+++
@@ -1,7 +0,0 @@
-class AddLatitudeAndLongitudeToModel < ActiveRecord::Migration[5.2]
-  def change
-    add_column :models, :latitude, :float
-    add_column :models, :longitude, :float
-    add_column :models, :address, :string
-  end
-end
--- db/migrate/20210318214335_create_latitude_and_longitudes.rb
+++
@@ -1,8 +0,0 @@
-class CreateLatitudeAndLongitudes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :latitude_and_longitudes do |t|
-
-      t.timestamps
-    end
-  end
-end
