e474c710fa932f7fe4077b48d8aa76d46bc15fef
sawaken-tech <sawapaca@gmail.com>
2021-04-05 06:40:50 +0900

    del

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,15 +1,3 @@
 class LocationsController < InheritedResources::Base
 
-  index
-    if params[:search].present?    @locations = Location.near(params[:search], 50,
-      :order => :distance)
-    else
-      @locations = Location.all
-    end
-  # private
-
-  #   def location_params
-  #     params.require(:location).permit(:address, :latitude, :longitude)
-  #   end
-
 end
