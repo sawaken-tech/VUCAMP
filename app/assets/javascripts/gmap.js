66eed6f4b6cda9bcf469054b63005389b3deeade
sawaken-tech <sawapaca@gmail.com>
2021-04-08 06:35:49 +0900

    fix

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -1,5 +1,5 @@
 function initMap(){
-  let map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: -34.397, lng: 150.644},
   zoom: 8
   });
